38583753f53251ff8a0ebf012cda35428166f538
Jovel960 <jovel960@icloud.com>
2023-05-19 13:59:32 +0300

    update

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const aboutRouter = require("./controllers/abous");
-const app = express();
 const middleWares = require("./utilities/middleware");
+const addCost = require("./controllers/addCost");
+const app = express();
+
+mongoose
+  .connect("mongodb+srv://jovelh960:jovelh960@cluster0.rqk3rwu.mongodb.net/costs?retryWrites=true&w=majority")
+  .then((res) => console.log("connected"))
+  .catch((err) => console.log(err,"error"));
 
 app.use(express.json());
-app.use('/about', aboutRouter);
-app.use(middleWares.unKnownEndPoint)
+app.use("/addcost", addCost);
+app.use("/about", aboutRouter);
+app.use(middleWares.unKnownEndPoint);
 
 module.exports = app;
