4838cb4d8fceef2c68e8ca0e7620ca0f7047467e
Jovel960 <jovel960@icloud.com>
2023-05-19 17:40:20 +0300

    added users route for dev

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const aboutRouter = require("./controllers/abous");
+const aboutRouter = require("./controllers/aboutUsRouter");
 const middleWares = require("./utilities/middleware");
-const addCost = require("./controllers/addCost");
+const usersRouter = require("./controllers/usersRouter");
+const addCost = require("./controllers/addCostRouter");
 const app = express();
 const config = require("./utilities/config");
 
@@ -15,6 +16,7 @@ mongoose
 app.use(express.json());
 app.use("/addcost", addCost);
 app.use("/about", aboutRouter);
+app.use("/api/users", usersRouter);
 app.use(middleWares.unKnownEndPoint);
 
 module.exports = app;
