ea1275aede8f7044b44cbbe286f1cfa95b3bd486
Jovel960 <jovel960@icloud.com>
2023-05-19 13:18:57 +0300

    Created the two schemas

--- models/costs.js
+++ models/costs.js
@@ -12,6 +12,7 @@ const costsSchema = new mongoose.Schema({
 
 costsSchema.set("toJSON", {
     transform:(document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
         delete returnedObject._id
         delete returnedObject.__v;
     }
