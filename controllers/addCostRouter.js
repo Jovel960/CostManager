5557e69178e6f65942ff728d842166470c0106e1
Jovel960 <jovel960@icloud.com>
2023-05-23 20:38:38 +0300

    added assertion for add cost if user not exist

--- controllers/addCostRouter.js
+++ controllers/addCostRouter.js
@@ -1,9 +1,12 @@
 const addCost = require("express").Router();
 const Cost = require("../models/costs");
+const User = require("../models/users");
 const { assertProps } = require("../utilities/middleware");
 
 addCost.post("/", assertProps, async (req, res, next) => {
   try {
+    const user = await User.findOne({ user_id: req.body.user_id });
+    if (!user) return res.status(400).json({ error: "user not exist" });
     const cost = new Cost(req.body);
     await cost.save();
     return res.status(201).json(cost);
@@ -18,6 +21,15 @@ addCost.get("/", async (req, res, next) => {
     return res.status(200).json(costs);
   } catch (e) {
     next();
+  }
+});
+
+addCost.delete("/", async (req, res, next) => {
+  try {
+    const costs = await Cost.deleteMany();
+    return res.status(200).json({ updated: true });
+  } catch (e) {
+    return res.status(400).json({ updated: false });
   }
 });
 
