1592ad0a7d91d7d0577bc3111551f5559c51fa43
Jovel960 <jovel960@icloud.com>
2023-05-23 22:37:31 +0300

    added report router

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const aboutRouter = require("./controllers/aboutUsRouter");
 const middleWares = require("./utilities/middleware");
 const usersRouter = require("./controllers/usersRouter");
-const addCost = require("./controllers/addCostRouter");
+const addCostRouter = require("./controllers/addCostRouter");
+const reportRouter = require("./controllers/reportRouter");
 const app = express();
 const config = require("./utilities/config");
 
@@ -13,9 +14,10 @@ mongoose
   .catch((err) => console.log(err, "error"));
 
 app.use(express.json());
-app.use("/addcost", addCost);
+app.use("/addcost", addCostRouter);
 app.use("/about", aboutRouter);
 app.use("/api/users", usersRouter);
+app.use("/report", reportRouter);
 app.use(middleWares.unKnownEndPoint);
 
 module.exports = app;
---
+++ controllers/reportRouter.js
@@ -0,0 +1,7 @@
+const reportRouter = require("express").Router();
+const User = require("../models/users");
+const Cost = require("../models/costs");
+
+//TODO
+
+module.exports = reportRouter;
