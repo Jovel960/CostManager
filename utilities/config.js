0744f31fb64f0c68b7e73b6d2ace04bb2204fd4f
Jovel960 <jovel960@icloud.com>
2023-06-20 11:04:53 +0300

    port update

--- utilities/config.js
+++ utilities/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 //Get the env mongo uri and port variables
 const MONGODB_URI = process.env.MONGODB_URI;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001
 
 module.exports = { MONGODB_URI, PORT };
